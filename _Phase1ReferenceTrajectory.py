a5315299089e9b95ca240e42ba3fba75d4d937f3
AquilaDrive <germandrives@gmail.com>
2026-07-27 16:01:12 +0500

    Update _Phase1ReferenceTrajectory.py

--- _Phase1ReferenceTrajectory.py
+++ _Phase1ReferenceTrajectory.py
@@ -203,19 +203,11 @@ class ReferenceTrajectoryGenerator:
         target_alt = curr_alt
         target_vsi = 0.0
         forced_turn_dir = "AUTO"
+
         for i in range(n):
             t_curr = timestamps[i]
 
-            # Continuously update reference turn rate based on current altitude
             active_turn_rate = self.calculate_turn_rate(curr_alt)
-
-            while (
-                event_idx < num_events
-                and t_curr >= atc_df.loc[event_idx, "Timestamp"]
-            ):
-
-        for i in range(n):
-            t_curr = timestamps[i]
 
             while (
                 event_idx < num_events
